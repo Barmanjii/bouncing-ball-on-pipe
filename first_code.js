f6ce95c8f21beb4856445673dbc79895a879abc6
Barmanjii <vbarman116@gmail.com>
2024-01-17 01:37:06 +0530

    lets start this journey

---
+++ first_code.js
@@ -0,0 +1,6 @@
+console.log("First project with first commit");
+
+// in this we will create a bouncing ball pipe game and will try to use the pure concept rather than using the framework or module will try to understand how things works. this will be the moded version of the game with crazy ass movements. with maybe more than 2-3 types 
+// 1. Base one with one pipe and wall bouncing game, in this the ball can bounce from the wall.
+// 2. We can add more pipe, like in the left and right and try to see how it works. in this ball will not bounce from the wall.
+// 3. Challenging one, where the wall pipe can be disappear and reappear at some certain amount of time for sure but it will be cool to built.
